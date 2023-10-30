89ed85aa427da7cd73402350e51d007121c7de9a
Julio Bermudez <julioph0n3@gmail.com>
2023-10-30 10:32:25 -0400

    tested all the functionability, just missing the README File and deploy

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,7 +29,7 @@ export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('tjate');
+  const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
